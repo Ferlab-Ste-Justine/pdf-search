d3c38ede7ad22cb8458e344ab928ad48278de922
Velythyl <charlie.gauthier@umontreal.ca>
2019-06-03 12:36:08 -0400

    tasks

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import java.io.{File, FileInputStream, InputStream}
 
+import Tasker._
+
 import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
@@ -64,59 +66,10 @@ object Main {
         esIndexer = new ESIndexer(argMap("esurl"))
         s3Downloader = new S3Downloader(argMap("bucket"))
 
-        import scala.concurrent.ExecutionContext.Implicits.global
-        import scala.concurrent.duration.Duration
-        import scala.concurrent.{Await, Future}
-
-        /**
-          * Transforms a call-by-name into a no-parameter function call to be able to use it in a varargs
-          *
-          * https://stackoverflow.com/questions/13307418/scala-variable-argument-list-with-call-by-name-possible
-          *
-          * @param a the call-by-name parameter
-          * @tparam A its return type
-          * @return the call by name parameter as a function
-          */
-        implicit def bynameToNoarg[A](a: => A): () => A = () => a
-
-        class Matcher[T]
-        object Matcher {
-            implicit object ListOfFutureWitness extends Matcher[List[Future[Any]]]
-            implicit object ListWitness extends Matcher[List[Any]]
-            implicit object FutureOfListWitness extends Matcher[Future[List[Any]]]
-            implicit object AnyWitness extends Matcher[Any]
-        }
-
-        /**
-          * Syntactic sugar to parallelize the execution of multiple function calls.
-          *
-          * Creates a Future per block of code to execute and then blocks until all the tasks are done.
-          *
-          * https://alvinalexander.com/scala/fp-book/how-to-use-by-name-parameters-scala-functions
-          *
-          * @param codes the function calls to parallelize
-          */
-        case class Tasks[B: Matcher](codes: (() => B)*) {
-            Await.result(
-                Future.traverse(codes) { code =>
-                    Future[Unit] {
-                        val result = code()
-
-                        result match {
-                            case a: List[Future[Any]] =>
-                                Await.result(Future.sequence(a), Duration.Inf)
-                            case _: List[Any] =>
-                            case a: Future[List[Any]] =>
-                                Await.result(a, Duration.Inf)
-                        }
-                    }
-                }
-                , Duration.Inf)
-        }
-
         val startTime = System.currentTimeMillis()
 
         Tasks(
+            Future[List[String]] { List("allo", "salut")} ,
             indexParticipants("https://kf-api-dataservice.kids-first.io", "/participants", "limit=100&visible=true"),
             if(argMap("do").equals("adminremote")) {
                 indexPDFRemote(argMap("starturl"), argMap("midurl"), argMap("endurl"))
