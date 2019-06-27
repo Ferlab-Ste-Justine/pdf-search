ef2590dbd44d76d7f05fa505d57a0a89d58f6de2
Velythyl <charlie.gauthier@umontreal.ca>
2019-06-27 14:53:30 -0400

    s3 region

--- src/main/scala/S3Downloader.scala
+++ src/main/scala/S3Downloader.scala
@@ -1,18 +1,18 @@
 import java.io.InputStream
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.model.{GetObjectRequest, S3Object}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 
 
-class S3Downloader(region: String = "us-east-1") {
+class S3Downloader {
   //https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/s3/transfer/TransferManager.html#downloadDirectory-java.lang.String-java.lang.String-java.io.File-
   //https://stackoverflow.com/questions/49116960/download-all-the-files-from-a-s3-bucket-using-scala
   //https://docs.aws.amazon.com/AmazonS3/latest/dev/RetrievingObjectUsingJava.html
 
   val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard()
-    .withRegion(region)
-    .withCredentials(new ProfileCredentialsProvider())
+    .withRegion(Regions.US_EAST_1)
     .build()
 
   def download(bucketName: String, key: String): InputStream = {
